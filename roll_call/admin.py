78b9417b9baa667c05bb91e77da9d513fbb01fe1
wilson8507 <wilson8507@gmail.com>
2017-04-17 23:16:24 +0800

    Complete

--- roll_call/admin.py
+++ roll_call/admin.py
@@ -3,6 +3,19 @@ from roll_call.models import set_number,set_time,attendence
 
 # Register your models here.
 
-admin.site.register(set_number)
-admin.site.register(set_time)
-admin.site.register(attendence)
+class attendenceAdmin(admin.ModelAdmin):
+	list_display=("student","attend","date")
+	list_filter=('date',)
+	search_field=['student','date']
+
+class set_timeAdmin(admin.ModelAdmin):
+		list_display=("start","end")
+		list_filter=('end',)
+
+class set_numberAdmin(admin.ModelAdmin):
+	list_display=("number","created_date")
+	list_filter=('created_date',)
+
+admin.site.register(set_number,set_numberAdmin)
+admin.site.register(set_time,set_timeAdmin)
+admin.site.register(attendence,attendenceAdmin)
